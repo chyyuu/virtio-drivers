263a20513a6b8eaea57e70c1de22637229141b6a
Yu Chen <yuchen@mail.tsinghua.edu.cn>
2021-03-06 21:19:57 +0800

    clean code for rustc 1.52 nighly

--- examples/riscv/src/main.rs
+++ examples/riscv/src/main.rs
@@ -1,8 +1,9 @@
 #![no_std]
 #![no_main]
 #![deny(warnings)]
+#![feature(llvm_asm)]
 
-#[macro_use]
+// #[macro_use]
 extern crate alloc;
 #[macro_use]
 extern crate log;
@@ -63,44 +64,44 @@ fn virtio_probe(node: &Node) {
         );
         info!("Device tree node {:?}", node);
         match header.device_type() {
-            DeviceType::Block => virtio_blk(header),
-            DeviceType::GPU => virtio_gpu(header),
+            //DeviceType::Block => virtio_blk(header),
+            //DeviceType::GPU => virtio_gpu(header),
             DeviceType::Input => virtio_input(header),
-            DeviceType::Network => virtio_net(header),
+            //DeviceType::Network => virtio_net(header),
             t => warn!("Unrecognized virtio device: {:?}", t),
         }
     }
 }
 
-fn virtio_blk(header: &'static mut VirtIOHeader) {
-    let mut blk = VirtIOBlk::new(header).expect("failed to create blk driver");
-    let mut input = vec![0xffu8; 512];
-    let mut output = vec![0; 512];
-    for i in 0..32 {
-        for x in input.iter_mut() {
-            *x = i as u8;
-        }
-        blk.write_block(i, &input).expect("failed to write");
-        blk.read_block(i, &mut output).expect("failed to read");
-        assert_eq!(input, output);
-    }
-    info!("virtio-blk test finished");
-}
+// fn virtio_blk(header: &'static mut VirtIOHeader) {
+//     let mut blk = VirtIOBlk::new(header).expect("failed to create blk driver");
+//     let mut input = vec![0xffu8; 512];
+//     let mut output = vec![0; 512];
+//     for i in 0..32 {
+//         for x in input.iter_mut() {
+//             *x = i as u8;
+//         }
+//         blk.write_block(i, &input).expect("failed to write");
+//         blk.read_block(i, &mut output).expect("failed to read");
+//         assert_eq!(input, output);
+//     }
+//     info!("virtio-blk test finished");
+// }
 
-fn virtio_gpu(header: &'static mut VirtIOHeader) {
-    let mut gpu = VirtIOGpu::new(header).expect("failed to create gpu driver");
-    let fb = gpu.setup_framebuffer().expect("failed to get fb");
-    for y in 0..768 {
-        for x in 0..1024 {
-            let idx = (y * 1024 + x) * 4;
-            fb[idx] = x as u8;
-            fb[idx + 1] = y as u8;
-            fb[idx + 2] = (x + y) as u8;
-        }
-    }
-    gpu.flush().expect("failed to flush");
-    info!("virtio-gpu test finished");
-}
+// fn virtio_gpu(header: &'static mut VirtIOHeader) {
+//     let mut gpu = VirtIOGpu::new(header).expect("failed to create gpu driver");
+//     let fb = gpu.setup_framebuffer().expect("failed to get fb");
+//     for y in 0..768 {
+//         for x in 0..1024 {
+//             let idx = (y * 1024 + x) * 4;
+//             fb[idx] = x as u8;
+//             fb[idx + 1] = y as u8;
+//             fb[idx + 2] = (x + y) as u8;
+//         }
+//     }
+//     gpu.flush().expect("failed to flush");
+//     info!("virtio-gpu test finished");
+// }
 
 fn virtio_input(header: &'static mut VirtIOHeader) {
     let mut event_buf = [0u64; 32];
@@ -113,11 +114,11 @@ fn virtio_input(header: &'static mut VirtIOHeader) {
     // TODO: handle external interrupt
 }
 
-fn virtio_net(header: &'static mut VirtIOHeader) {
-    let mut net = VirtIONet::new(header).expect("failed to create net driver");
-    let mut buf = [0u8; 0x100];
-    let len = net.recv(&mut buf).expect("failed to recv");
-    info!("recv: {:?}", &buf[..len]);
-    net.send(&buf[..len]).expect("failed to send");
-    info!("virtio-net test finished");
-}
+// fn virtio_net(header: &'static mut VirtIOHeader) {
+//     let mut net = VirtIONet::new(header).expect("failed to create net driver");
+//     let mut buf = [0u8; 0x100];
+//     let len = net.recv(&mut buf).expect("failed to recv");
+//     info!("recv: {:?}", &buf[..len]);
+//     net.send(&buf[..len]).expect("failed to send");
+//     info!("virtio-net test finished");
+// }
